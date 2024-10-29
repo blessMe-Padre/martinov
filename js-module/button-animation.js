2b1632cb5a858b6d250e36f76dbccd1ba659a414
Сергей Субботин <zarodiny@yandex.ru>
2024-10-29 12:00:26 +1000

    feat: добавляет стили

--- js-module/button-animation.js
+++ js-module/button-animation.js
@@ -1,44 +1,43 @@
 export const initButtonAnimation = () => {
-    const button = document.querySelector('._anim-button');
-    const text = button.textContent.trim();
-    button.innerHTML = '';
-    let indexWithoutSpaces = 0;
+    const buttons = document.querySelectorAll('._anim-button');
+    buttons.forEach((button) => {
+        const text = button.textContent.trim();
+        button.innerHTML = '';
+        let indexWithoutSpaces = 0;
 
-    text.split('').forEach(char => {
-        const span = document.createElement('span');
+        text.split('').forEach(char => {
+            const span = document.createElement('span');
 
-        if (char === ' ') {
-            // span.classList.add('space');
-            span.innerHTML = '&nbsp;';
-        } else {
-            span.textContent = char;
-
-            if (indexWithoutSpaces % 2 === 0) {
-                span.classList.add('even');
+            if (char === ' ') {
+                // span.classList.add('space');
+                span.innerHTML = '&nbsp;';
             } else {
-                span.classList.add('odd');
-            }
-            indexWithoutSpaces++;
-        }
+                span.textContent = char;
 
-        button.appendChild(span);
-    });
-
-    button.addEventListener('mouseover', () => {
-        button.querySelectorAll('span').forEach((span, index) => {
-            if (!span.classList.contains('space')) { // Пропускаем пробелы
-                span.classList.add('animate');
-                // span.style.animationDelay = `${index * 50}ms`;
+                if (indexWithoutSpaces % 2 === 0) {
+                    span.classList.add('even');
+                } else {
+                    span.classList.add('odd');
+                }
+                indexWithoutSpaces++;
             }
+            button.appendChild(span);
+        });
+
+        button.addEventListener('mouseover', () => {
+            button.querySelectorAll('span').forEach((span, index) => {
+                if (!span.classList.contains('space')) { // Пропускаем пробелы
+                    span.classList.add('animate');
+                    // span.style.animationDelay = `${index * 50}ms`;
+                }
+            });
+        });
+        button.addEventListener('mouseleave', () => {
+            button.querySelectorAll('span').forEach(span => {
+                span.classList.remove('animate');
+                span.style.animationDelay = '';
+            });
         });
     });
-
-    button.addEventListener('mouseleave', () => {
-        button.querySelectorAll('span').forEach(span => {
-            span.classList.remove('animate');
-            span.style.animationDelay = '';
-        });
-    });
-
 
 }
