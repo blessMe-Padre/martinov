c7786350fa32b81d303f8436a0a0c483b48710a7
Сергей Субботин <zarodiny@yandex.ru>
2024-10-29 14:24:11 +1000

    feat: добавляет стили футтер

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { initNav } from './js-module/nav';
 import { initAccordions } from './js-module/acoordions';
 import { initSlider } from './js-module/sliders';
 import { initButtonAnimation } from './js-module/button-animation';
+import { initCurrentYear } from './js-module/current-year';
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('подключен скрипт main.js');
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   initAccordions();
   initSlider();
   initButtonAnimation();
+  initCurrentYear();
   // DOMContentLoaded
 
 });
